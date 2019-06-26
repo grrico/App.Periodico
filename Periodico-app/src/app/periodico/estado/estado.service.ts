81fb3425967962c1a94d1384a5aea1d9466512a0
Genaro Rico <grrico@hotmail.com>
2019-06-26 07:35:57 -0500

    Prueba

--- Periodico-app/src/app/periodico/estado/estado.service.ts
+++ Periodico-app/src/app/periodico/estado/estado.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 // se add el HttpClient
 import {HttpClient} from '@angular/common/http';
 
+// carga la ulr de environments
+import { environment} from 'src/environments/environment'
+
 // importa la clase estado
 import {PeriodicoEstadoList} from './estados'
 
@@ -13,12 +16,15 @@ import {PeriodicoEstadoList} from './estados'
 //PeriodicoEstadoList
 export class PeridicoEstadoServiceApi {
 
+  uriApi:string;
 
   constructor(private httpClient: HttpClient) { 
     console.log('service esta trabajando el servicio estado!');
+    this.uriApi=environment.UrlServiceJson;
   }
   getDataEstadoAll(){
-    return this.httpClient.get<PeriodicoEstadoList[]>('http://localhost/Periodico.WebApi/EstadoGetAll');
+    //return this.httpClient.get<PeriodicoEstadoList[]>('http://localhost/Periodico.WebApi/EstadoGetAll');
+    return this.httpClient.get<PeriodicoEstadoList[]>(this.uriApi);
   }
   
 }
